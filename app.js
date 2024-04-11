0aaeb84304a3cbf836e6c841cedc94c27d9b1885
gitpersonaluse <gitpersonaluse@gmail.com>
2024-04-11 10:09:18 +0530

    hello this is a messege

---
+++ app.js
@@ -0,0 +1,26 @@
+// let para = document.getElementsById("test")
+let para = document.getElementsByTagName("p")
+let btn = document.getElementById("button")
+
+btn.addEventListener("click",changethetext )
+
+
+function changethetext(){    
+     para[1].innerHTML = "hello "
+
+}
+
+
+
+
+zyx = {
+    add:function(a,b){
+        console.log(a+b);
+    }
+}
+
+//modified
+//staged
+//unmodified
+
+
